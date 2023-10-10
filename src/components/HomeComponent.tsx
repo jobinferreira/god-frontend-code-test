c5538608c4f3bd6a51324762cee0d581934adbff
jobinferreira <jobin.ferreira@gmail.com>
2023-10-09 21:42:00 -0300

    feat: add mobile pagination

--- src/components/HomeComponent.tsx
+++ src/components/HomeComponent.tsx
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { Text } from 'vcc-ui';
 import { useCars } from '../hooks/useCars';
 import { CarCard } from "./CarCard";
 import styles from '../../public/css/home.module.css'
 import Spacer from "./Spacer";
 import ButtonsPages from "./PaginationDesktop";
+import PaginationMobile from "./PaginationMobile";
 
 export const HomeComponent: React.FC = () => {
 
   const {cars}  = useCars();
+
+  const [selected, setSelected] = useState(0);
 
   const onClickNavigate = (left: boolean) => {
     let cardList = document.getElementById("card-list");
@@ -17,10 +20,19 @@ export const HomeComponent: React.FC = () => {
     let scrollSize = cardList?.scrollWidth ?? 0;
     let scrollPosition = cardList?.scrollLeft ?? 0;
 
-    console.log({scrollSize, scrollPosition, cardSize})
-
     if(left) cardList?.scrollTo({ left: scrollPosition - cardSize})
     else cardList?.scrollTo({ left: scrollPosition + cardSize})
+  }
+
+  const onClickMobile = (index: number) => {
+    let cardList = document.getElementById("card-list");
+    let card = cardList?.firstElementChild;
+    let cardSize = (card?.clientWidth ?? 0) + 24;
+    let scrollSize = cardList?.scrollWidth ?? 0;
+    let scrollPosition = cardList?.scrollLeft ?? 0;
+
+    cardList?.scrollTo({ left: scrollPosition + cardSize})
+    setSelected(index);
   }
 
   return (
@@ -33,7 +45,10 @@ export const HomeComponent: React.FC = () => {
         car={car}
         />)}
       </div>
-      <ButtonsPages onClickLeft={() => onClickNavigate(true)} onClickRight={() => onClickNavigate(false)}/>
+      <ButtonsPages 
+        onClickLeft={() => onClickNavigate(true)} 
+        onClickRight={() => onClickNavigate(false)}/>
+      <PaginationMobile selected={selected} onClick={onClickMobile} total={cars.length}/> 
     </div>
     )
 };
